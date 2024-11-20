d0db4035de5c5d28a658446a11497a8de9f7a130
Hadeel Moustafa <hadeel94.9@hotmail.com>
2024-11-20 14:36:48 +0100

    Pikle file

---
+++ Other/Pikle.py
@@ -0,0 +1,9 @@
+# import pickle
+# a_file = open("datafull.pkl", "wb")
+# pickle.dump(score_data, a_file)
+# a_file.close()
+
+
+# a_file = open("data.pkl", "rb")
+# output = pickle.load(a_file)
+# output['100-dot-0']
